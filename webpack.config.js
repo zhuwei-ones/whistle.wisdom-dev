f4aed9daf2b24d7a6c1ccc07c12cf7b3015bfa43
zhuwei-ones <zhuwei@ones.cn>
2022-11-22 10:57:14 +0800

    build: 打包初始配置

---
+++ webpack.config.js
@@ -0,0 +1,119 @@
+const Webpack = require("webpack");
+const Path = require("path");
+const { execSync } = require("child_process");
+const sveltePreprocess = require("svelte-preprocess");
+const pkg = require("./package.json");
+
+module.exports = (_, argv) => {
+  const isDev = argv.mode === "development";
+
+  const plugins = [
+    new Webpack.DefinePlugin({
+      __VERSION__: JSON.stringify(pkg.version),
+    }),
+  ];
+
+  if (isDev) {
+    plugins.push({
+      apply: (compiler) => {
+        compiler.hooks.done.tap("DeclarationEmitter", () => {
+          //   execSync("npm run build:typings");
+          console.log("ok");
+        });
+      },
+    });
+  }
+
+  return {
+    mode: argv.mode,
+    devtool: false,
+    entry: {
+      index: Path.resolve(__dirname, "./src/client/index.ts"),
+    },
+    target: ["web", "es5"],
+    output: {
+      path: Path.resolve(__dirname, "./dist/client"),
+      filename: "wisdom-dev.min.js",
+      library: {
+        name: "wisdom-dev",
+        type: "umd",
+        umdNamedDefine: true,
+        export: "default",
+      },
+      globalObject: "this || self",
+    },
+    resolve: {
+      extensions: [".css", ".ts", ".js", ".html", ".less", ".mjs", ".svelte"],
+      alias: {
+        svelte: Path.resolve("node_modules", "svelte"),
+        const: Path.resolve("src/client/const"),
+        lib: Path.resolve("src/client/lib"),
+        components: Path.resolve("src/client/components"),
+      },
+      mainFields: ["svelte", "browser", "module", "main"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|ts)$/,
+          use: [{ loader: "babel-loader" }],
+        },
+        {
+          test: /\.(less|css)$/i,
+          use: [
+            {
+              loader: "style-loader",
+              options: { injectType: "lazyStyleTag" },
+            },
+            { loader: "css-loader" },
+            {
+              loader: "less-loader",
+              options: {
+                lessOptions: { math: "always" },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(svelte)$/,
+          use: [
+            "babel-loader",
+            {
+              loader: "svelte-loader",
+              options: {
+                preprocess: sveltePreprocess({
+                  sourceMap: isDev,
+                }),
+                compilerOptions: {
+                  dev: isDev,
+                  accessors: true,
+                },
+                emitCss: true,
+                hotReload: false,
+              },
+            },
+          ],
+        },
+        {
+          // required to prevent errors from Svelte on Webpack 5+, omit on Webpack 4
+          test: /node_modules[\\/]svelte[\\/].*\.m?js$/,
+          resolve: {
+            fullySpecified: false,
+          },
+          use: ["babel-loader"],
+        },
+      ],
+    },
+    stats: {
+      colors: true,
+      errorDetails: true,
+    },
+    optimization: {
+      minimize: !isDev,
+    },
+    watchOptions: {
+      ignored: ["**/node_modules"],
+    },
+    plugins,
+  };
+};
---
+++ webpack.serve.config.js
@@ -0,0 +1,86 @@
+const Path = require("path");
+const fs = require("fs");
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const baseConfig = require("./webpack.config.js");
+
+const contentBase = Path.join(__dirname, "/");
+
+module.exports = (env, argv) => {
+  return merge(baseConfig(env, argv), {
+    mode: "development",
+    // devtool: 'eval-source-map',
+    devtool: false,
+    devServer: {
+      host: "0.0.0.0",
+      port: 9191,
+      open: "dist/client/index.html",
+      allowedHosts: "all",
+      historyApiFallback: true,
+      client: {
+        overlay: true,
+      },
+      static: [{ directory: contentBase }],
+      onBeforeSetupMiddleware(devServer) {
+        devServer.app.all("*", (req, res) => {
+          const contentType = {
+            flv: "video/x-flv",
+            wav: "audio/x-wav",
+          };
+          const fileType = req.path.split(".").pop();
+          // console.log('Req:::', fileType, req.path, req.query);
+          if (fileType === "flv") {
+            res.set({
+              "Content-Type": contentType[fileType],
+              // 'Content-Type', 'application/octet-stream',
+              "Transfer-Encoding": "chunked",
+              Connection: "keep-alive",
+            });
+            let n = 0;
+            const write = () => {
+              setTimeout(() => {
+                n++;
+                const buf = Buffer.alloc(100000, 1);
+                res.write(buf);
+                if (n < 100) {
+                  write();
+                } else {
+                  res.end();
+                }
+              }, 100);
+            };
+            write();
+          } else {
+            const delay = req.query.t || Math.ceil(Math.random() * 100);
+            setTimeout(() => {
+              const filePath = Path.join(contentBase, req.path);
+              try {
+                fs.accessSync(filePath, fs.constants.F_OK);
+                res.type(fileType);
+                res.status(req.query.s || 200);
+                if (req.query.chunked) {
+                  res.set({
+                    "Transfer-Encoding": "chunked",
+                    Connection: "keep-alive",
+                  });
+                  res.write(fs.readFileSync(filePath));
+                } else {
+                  res.send(fs.readFileSync(filePath));
+                }
+                // res.sendFile(filePath, options);
+              } catch (e) {
+                res.status(404);
+                res.end();
+              }
+              // console.log(req.query);
+            }, delay);
+          }
+        });
+      },
+    },
+    optimization: {
+      minimize: false,
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+  });
+};
