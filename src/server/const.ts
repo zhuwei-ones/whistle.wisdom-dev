6e826bd6ce4595f2846a6e9096ac87590572c849
zhuwei-ones <zhuwei@ones.cn>
2022-11-22 18:53:55 +0800

    chore(server): 添加 常量

---
+++ src/server/const.ts
@@ -0,0 +1,12 @@
+export const EnvList = ["cn", "com", "cnp", "comp"] as const;
+
+export const LanguageList = ["zh", "ja", "en"] as const;
+
+export const PageAllowHost = [
+  "myones.net",
+  "ones.com",
+  "ones.cn",
+  "localhost",
+  "127.0.0.1",
+  "our.ones.pro",
+];
