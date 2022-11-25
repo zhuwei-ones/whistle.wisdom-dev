42780730e7414bd07fa68d01ecd508610855dfd4
zhuwei-ones <zhuwei@ones.cn>
2022-11-25 11:30:21 +0800

    chore: 接入 eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./src/client/tsconfig.json","./src/server/tsconfig.json"],
+  },
+  rules: {
+    "no-continue": "off",
+    "no-console": "off",
+  },
+  ignorePatterns: ["__tests__/**", "dist/**", "src/**/*.test.js"],
+};
