0644b8c7c63f347a72e0ccd84ec33255555c0d4e
zhuwei-ones <zhuwei@ones.cn>
2022-11-25 19:50:07 +0800

    chore: svelte 接入eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,20 @@ module.exports = {
     "prettier"
   ],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  overrides: [],
+  plugins: ["svelte3", "@typescript-eslint"],
+  overrides: [
+    {
+      files: ["*.svelte"],
+      processor: "svelte3/svelte3"
+    }
+  ],
+
+  settings: {
+    "svelte3/typescript": require("typescript"),
+    // ignore style tags in Svelte because of Tailwind CSS
+    // See https://github.com/sveltejs/eslint-plugin-svelte3/issues/70
+    "svelte3/ignore-styles": () => true
+  },
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
